39c223b009939883bd6f50ca94b9f556777f9b2f
Darshan3690 <darshan.rajput123091@marwadiuniversity.ac.in>
2026-05-28 23:06:47 +0530

    Fix CORS: Allow X-Due-Date header from frontend

--- apps/server/src/index.ts
+++ apps/server/src/index.ts
@@ -66,7 +66,7 @@ app.use(
     origin: webUrl,
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE", "PATCH"],
-    allowedHeaders: ["Content-Type", "Authorization"],
+    allowedHeaders: ["Content-Type", "Authorization", "X-Due-Date"],
   })
 );
 app.use(express.json());
